80469fb0bb6a46298d9981d3558fe2eb6a17c3dc
azxki <azxki01@gmail.com>
2023-01-04 16:39:50 +0100

    exo

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-window.onload = () => {
-    // On récupère tous les boutons d'ouverture de modale
-    const modalButtons = document.querySelectorAll("[data-toggle=modal]");
 
-    for(let button of modalButtons){
-        button.addEventListener("click", function(e){
-            // On empêche la navigation
-            e.preventDefault();
-            // On récupère le data-target
-            let target = this.dataset.target
 
-            // On récupère la bonne modale
-            let modal = document.querySelector(target);
-            // On affiche la modale
-            modal.classList.add("show");
+let newWin = window.open("about:blank", "hello", "width=500,height=500");
+newWin.focus();
+newWin.moveBy(700,200);
 
-            // On récupère les boutons de fermeture
-            const modalClose = modal.querySelectorAll("[data-dismiss=dialog]");
+newWin.document.write("Voulez-vous continuer en acceptant les cookies ? <br>" +
+    "<button>Refuser</button>" + "<button>Accepter</button>");
 
-            for(let close of modalClose){
-                close.addEventListener("click", () => {
-                    modal.classList.remove("show");
-                });
-            }
-
-            // On gère la fermeture lors du clic sur la zone grise
-            modal.addEventListener("click", function(){
-                this.classList.remove("show");
-            });
-            // On évite la propagation du clic d'un enfant à son parent
-            modal.children[0].addEventListener("click", function(e){
-                e.stopPropagation();
-            })
-        });
-    }
-
-}
+newWin.addEventListener('click', function (){
+    newWin.close();
+    document.body.style.filter = "none";
+})
