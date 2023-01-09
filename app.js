3371f82472ac5767f995f50ca8effcab4ae17af1
azxki <cococoursan@gmail.com>
2023-01-09 15:15:53 +0100

    exo

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ let newWin = window.open("about:blank", "hello", "width=500,height=500");
 newWin.focus();
 newWin.moveBy(700,200);
 
-newWin.document.write("Voulez-vous continuer en acceptant les cookies ? <br>" +
-    "<button>Refuser</button>" + "<button>Accepter</button>");
-
+newWin.document.write("Voulez-vous accéder à la page du site non floutée ? <br>" +
+    "<button id='refuser'>Non</button>" + "<button id='accepter'>Oui</button>");
 newWin.addEventListener('click', function (){
     newWin.close();
     document.body.style.filter = "none";
